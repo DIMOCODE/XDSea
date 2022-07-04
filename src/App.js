c1340df5dbe4e4677ea530ccd56b3afa29d99f99
PaulBG <paulantonio.bg@gmail.com>
2022-07-04 18:48:38 -0500

    Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
 import web3Context from "./context/web3Context";
+import ReactGA from 'react-ga';
 
 import NFTApp from "./nft-app";
 import { createContext } from "react";
+
+const TRACKING_ID = "UA-105859386-2"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 export class App extends Component {
   constructor(props) {
@@ -14,6 +18,8 @@ export class App extends Component {
       address: null,
     };
   }
+
+
 
   render() {
     return (
