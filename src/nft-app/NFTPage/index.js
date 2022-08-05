6e049465f32686829cb1fc4e5e4719cb713fe075
PaulBG <paulantonio.bg@gmail.com>
2022-08-05 17:11:00 -0500

    Video Component Alternative

--- src/nft-app/NFTPage/index.js
+++ src/nft-app/NFTPage/index.js
@@ -110,6 +110,8 @@ import {
   withdrawOfferRequest,
 } from "../../API/NFT";
 
+import { useVideo } from "react-use";
+
 const NFTDetails = (props) => {
   const webLocation = useLocation();
   const history = useHistory();
@@ -753,6 +755,10 @@ const NFTDetails = (props) => {
     }, 1500);
   };
 
+  // Alternative Video Component https://github.com/streamich/react-use/blob/master/docs/useVideo.md
+
+  const [video, state, controls, ref] = useVideo(<video src={nft?.image} />);
+
   return (
     <NFTPage>
       {processingOffer ? (
@@ -1106,8 +1112,11 @@ const NFTDetails = (props) => {
                             </Lock>
                           </AnimatePresence>
                         ) : null}
-                        <ReactPlayer
-                          url={nft?.image}
+                        {/* <ReactPlayer
+                          // url={nft?.image}
+                          url={
+                            "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4"
+                          }
                           playing={true}
                           muted={true}
                           volume={0}
@@ -1115,7 +1124,35 @@ const NFTDetails = (props) => {
                           controls={true}
                           width="100%"
                           height="100%"
-                        />
+                        /> */}
+
+                        <div>
+                          {video}
+                          <pre>{JSON.stringify(state, null, 2)}</pre>
+                          <button onClick={controls.pause}>Pause</button>
+                          <button onClick={controls.play}>Play</button>
+                          <br />
+                          <button onClick={controls.mute}>Mute</button>
+                          <button onClick={controls.unmute}>Un-mute</button>
+                          <br />
+                          <button onClick={() => controls.volume(0.1)}>
+                            Volume: 10%
+                          </button>
+                          <button onClick={() => controls.volume(0.5)}>
+                            Volume: 50%
+                          </button>
+                          <button onClick={() => controls.volume(1)}>
+                            Volume: 100%
+                          </button>
+                          <br />
+                          <button onClick={() => controls.seek(state.time - 5)}>
+                            -5 sec
+                          </button>
+                          <button onClick={() => controls.seek(state.time + 5)}>
+                            +5 sec
+                          </button>
+                        </div>
+
                         {console.log(ReactPlayer.canPlay(nft?.image))}
                       </VStack>
                     ) : isAudio(nft?.fileType) ? (
