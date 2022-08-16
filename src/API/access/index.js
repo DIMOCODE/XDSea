1cc726f5b1ad9dd722275b87ba8de3296bc56c85
DIMOCODE <info@dimo.finance>
2022-08-16 18:01:59 +1200

    Revert "Merge branch 'UI-Integration' of https://github.com/DIMOCODE/XDSea into UI-Integration"
    This reverts commit a6aca664cbc1d631ebfaff1d6fdc1209b5d6fff3, reversing
    changes made to 5a752d266b9a45c2897f578268bdca3469230cf1.

--- src/API/access/index.js
+++ src/API/access/index.js
@@ -1,20 +1,12 @@
 import { createRequest } from "..";
 import { HTTP_METHODS, LS } from "../../constant";
-/**
- * This function consumes the web servies for anonymous login and return the user information and tokens
- * @param  {String} wallet the wallet user
- * @return {[Promise<Any>]}      Axios Promise Response
- */
+
 export const anonymousLogin = (wallet) => {
   const body = {
     XDCWallet: wallet,
   };
   return createRequest(HTTP_METHODS.post, "anonymous/access", null, body);
 };
-/**
- * This function clean the user data from the local storage
- * @return { void }      none
- */
 export const logout = () => {
   LS.removeAll();
 };
