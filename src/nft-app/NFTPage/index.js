7ee2077c3584c5e315ab06ab33135bac4b40ed5d
DIMOCODE <info@dimo.finance>
2022-08-16 18:36:03 +1200

    Revert "Merge branch 'UI-Integration' of https://github.com/DIMOCODE/XDSea into UI-Integration"
    This reverts commit 648d38626ab3743dfc68d77fd9d064bd444efbb7, reversing
    changes made to 8a378e680d00b33fc8b062431ac2d6fcfcf9b901.

--- src/nft-app/NFTPage/index.js
+++ src/nft-app/NFTPage/index.js
@@ -107,8 +107,6 @@ import {
   withdrawOfferRequest,
 } from "../../API/NFT";
 
-import { useVideo } from "react-use";
-
 const NFTDetails = (props) => {
   const webLocation = useLocation();
   const [wallet, setWallet] = useState(null);
@@ -675,10 +673,6 @@ const NFTDetails = (props) => {
     }, 1500);
   };
 
-  // Alternative Video Component https://github.com/streamich/react-use/blob/master/docs/useVideo.md
-
-  const [video, state, controls, ref] = useVideo(<video src={nft?.image} />);
-
   return (
     <NFTPage>
       {processingOffer ? (
@@ -1051,33 +1045,6 @@ const NFTDetails = (props) => {
                           width="100%"
                           height="100%"
                         />
-
-                        {/* <div>
-                          {video}
-                          <pre>{JSON.stringify(state, null, 2)}</pre>
-                          <button onClick={controls.pause}>Pause</button>
-                          <button onClick={controls.play}>Play</button>
-                          <br />
-                          <button onClick={controls.mute}>Mute</button>
-                          <button onClick={controls.unmute}>Un-mute</button>
-                          <br />
-                          <button onClick={() => controls.volume(0.1)}>
-                            Volume: 10%
-                          </button>
-                          <button onClick={() => controls.volume(0.5)}>
-                            Volume: 50%
-                          </button>
-                          <button onClick={() => controls.volume(1)}>
-                            Volume: 100%
-                          </button>
-                          <br />
-                          <button onClick={() => controls.seek(state.time - 5)}>
-                            -5 sec
-                          </button>
-                          <button onClick={() => controls.seek(state.time + 5)}>
-                            +5 sec
-                          </button>
-                        </div> */}
                       </VStack>
                     ) : isAudio(nft?.fileType) ? (
                       <VStack
