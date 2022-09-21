a9d32543b435fe41139e04f0d11a91049c99473b
DIMOCODE <info@dimo.finance>
2022-09-22 00:31:45 +1200

    Temporarily disabling Collection Filters on User Profile page

--- src/nft-app/MyNFT/index.js
+++ src/nft-app/MyNFT/index.js
@@ -2376,7 +2376,7 @@ const MyNFT = (props) => {
                             switched={subMenu === 1}
                             maxPrice={highestPrice}
                           ></FiltersButton>
-                          <Swiper
+                          {/* <Swiper
                             spaceBetween={0}
                             slidesPerView={"auto"}
                             grabCursor={true}
@@ -2412,7 +2412,7 @@ const MyNFT = (props) => {
                                   </SwiperSlide>
                                 ))
                               : null}
-                          </Swiper>
+                          </Swiper> */}
                           <SortButtonNFTS
                             onChange={handleChangeFilterNFT}
                             params={nftParams}
