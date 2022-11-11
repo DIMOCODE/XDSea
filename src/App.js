8f28c8f518b862e3a6d09e5e4c13e4c16761de57
DIMOCODE <info@dimo.finance>
2022-11-11 14:57:53 +1300

    Added GTM for analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
-import ReactGA from 'react-ga';
+import TagManager from "react-gtm-module";
 import NFTApp from "./nft-app";
 
-{/* Google Analytics Tracking ID */}
-const TRACKING_ID = "UA-105859386-2"; // OUR_TRACKING_ID
-ReactGA.initialize(TRACKING_ID);
+{/* Google Tag Manager ID */}
+const TAG_ID = "G-ET0G4GNEPM";
+TagManager.initialize({gtmId: TAG_ID});
 
 export class App extends Component {
   constructor(props) {
